5bfe45aceded8a636d9c879f4fcc923109ac3b7c
Almeida <diogoalmeida1104@gmail.com>
2020-04-14 16:13:45 +0100

    solved

---
+++ ex06_objetos.js
@@ -0,0 +1,13 @@
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+    };
+    function multiplyNumeric(menu) {
+        for (const key in menu) {
+            if(typeof(menu[key]) == "number") {
+                menu[key] = menu[key] * 2
+            }
+        }
+        console.table(menu)
+    }
