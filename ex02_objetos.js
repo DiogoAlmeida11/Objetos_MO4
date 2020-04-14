2a406ecaa96e3cf796eb6883368d89137527cabd
Almeida <diogoalmeida1104@gmail.com>
2020-04-14 10:57:48 +0100

    solved

--- ex02_objetos.js
+++ ex02_objetos.js
@@ -3,8 +3,21 @@ const student = {
     course: "POO",
     grade: 12
     }
-function listarObjetos (){
+listObjectNames()
+function listObjectNames() {
     for (const key in student) {
         console.log(key)                
     }    
 }
+//console.log(student.grade);
+//delete student.grade
+//console.log(student.grade);
+
+function calcObjectSize() {
+    let count = 0
+    for (const key in student) {
+        count++                                
+    }
+    console.log(count)
+}
+
